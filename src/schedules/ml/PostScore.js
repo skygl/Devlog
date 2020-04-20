d6e7ed0d395437b61e3ceddc2ce122e33f46af55
skygl <jupiter9501@gmail.com>
2020-04-20 22:58:37 +0900

    feat: 머신러닝을 활용하여 점수를 예측하는 기능 추가

---
+++ src/schedules/ml/PostScore.js
@@ -0,0 +1,133 @@
+import * as tf from '@tensorflow/tfjs';
+import DomService from "../../services/dom/DomService";
+import fs from 'fs';
+import path from 'path';
+import 'tfjs-node-save';
+
+const SCORE_CLASSES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const SCORE_COUNT = SCORE_CLASSES.length;
+const TAGS = ['h1', 'h2', 'h3', 'p', 'code', 'img', 'ul', 'ol', 'li', 'a', 'blockquote', 'table'];
+
+let model;
+
+async function saveModel(model) {
+    return model.save('file://' + __dirname + '/model');
+}
+
+export async function testModel(testDomInfo) {
+    if (!model) {
+        if (!fs.existsSync(path.resolve(__dirname, 'model/model.json'))) {
+            const [xTrain, yTrain, xTest, yTest] = await getDomData(.2);
+
+            model = await trainModel(xTrain, yTrain, xTest, yTest);
+
+            await saveModel(model);
+        } else {
+            model = await tf.loadLayersModel('file://' + __dirname + '/model/model.json');
+        }
+    }
+    const data = [];
+
+    for (const tag of TAGS) {
+        data.push(testDomInfo[tag] ? testDomInfo[tag] : 0);
+    }
+
+    const inputData = tf.tensor2d(data, [1, data.length]);
+    const prediction = await model.predict(inputData);
+
+    console.log("prediction", prediction.argMax(-1).dataSync());
+}
+
+async function trainModel(xTrain, yTrain, xTest, yTest) {
+    const model = tf.sequential();
+    const learningRate = .01;
+    const numberOfEpochs = 100;
+    const optimizer = tf.train.adam(learningRate);
+
+    model.add(tf.layers.dense(
+        {units: 100, activation: "relu", inputShape: [xTrain.shape[1]]}
+    ));
+    model.add(tf.layers.dense(
+        {units: 100, activation: 'relu'}
+    ));
+    model.add(tf.layers.dense(
+        {units: SCORE_COUNT, activation: 'softmax'}
+    ));
+
+    model.compile(
+        {optimizer: optimizer, loss: 'categoricalCrossentropy', metrics: ['accuracy']}
+    );
+
+    await model.fit(xTrain, yTrain, {
+        epochs: numberOfEpochs,
+        validationData: [xTest, yTest]
+    });
+
+    return model;
+}
+
+function convertToTensors(data, target, testSplit) {
+    const numExamples = data.length;
+    if (numExamples !== target.length) {
+        throw new Error("data and split have different numbers of example.");
+    }
+
+    const numTestExamples = Math.round(numExamples * testSplit);
+    const numTrainExamples = numExamples - numTestExamples;
+
+    const xDims = data[0].length;
+
+    const xs = tf.tensor2d(data, [numExamples, xDims]);
+    const ys = tf.oneHot(tf.tensor1d(target).toInt(), SCORE_COUNT);
+
+    const xTrain = xs.slice([0, 0], [numTrainExamples, xDims]);
+    const xTest = xs.slice([numTrainExamples, 0], [numTestExamples, xDims]);
+    const yTrain = ys.slice([0, 0], [numTrainExamples, SCORE_COUNT]);
+    const yTest = ys.slice([numTrainExamples, 0], [numTestExamples, SCORE_COUNT]);
+    return [xTrain, yTrain, xTest, yTest];
+}
+
+const getDomData = (async (testSplit) => {
+    return tf.tidy(() => {
+        const dataByScore = [];
+        const targetsByScore = [];
+
+        for (let i = 0; i < SCORE_COUNT; i++) {
+            dataByScore.push([]);
+            targetsByScore.push([]);
+        }
+
+        return DomService.findDom()
+            .then(doms => {
+                doms.forEach(dom => {
+                    const target = dom.score;
+                    const data = [];
+                    for (const tag of TAGS) {
+                        data.push(!dom[tag] ? 0 : dom[tag]);
+                    }
+                    dataByScore[target - 1].push(data);
+                    targetsByScore[target - 1].push(target);
+                });
+
+                const xTrains = [];
+                const yTrains = [];
+                const xTests = [];
+                const yTests = [];
+
+                for (let i = 0; i < SCORE_COUNT; i++) {
+                    const [xTrain, yTrain, xTest, yTest] = convertToTensors(dataByScore[i], targetsByScore[i], testSplit);
+                    xTrains.push(xTrain);
+                    yTrains.push(yTrain);
+                    xTests.push(xTest);
+                    yTests.push(yTest);
+                }
+
+                const concatAxis = 0;
+
+                return [
+                    tf.concat(xTrains, concatAxis), tf.concat(yTrains, concatAxis),
+                    tf.concat(xTests, concatAxis), tf.concat(yTests, concatAxis)
+                ];
+            });
+    });
+});
