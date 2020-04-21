58477f6c59e0cc89765263b2ea0f9428312c0192
skygl <jupiter9501@gmail.com>
2020-04-21 21:51:15 +0900

    feat: 날짜를 구하는 유틸 함수 추가

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -2,4 +2,14 @@ const copy = (o) => {
     return JSON.parse(JSON.stringify(o));
 };
 
+const getDate = (fromDate, {day = 0, hours = 0, min = 0, sec = 0, ms = 0}) => {
+    let target = new Date(fromDate);
+
+    target.setDate(fromDate.getDate() + day);
+    target.setHours(hours, min, sec, ms);
+
+    return target;
+};
+
 module.exports = copy;
+module.exports = getDate;
