d5282c9e2be3698e6aab5006620e0969a002a192
skygl <jupiter9501@gmail.com>
2020-04-24 20:46:58 +0900

    refactor: DomCrawler Dom 서비스와 모듈 분리

---
+++ src/modules/DomCrawler.js
@@ -0,0 +1,57 @@
+import cheerio from "cheerio";
+
+const crawlDom = async (url, domInfo) => {
+    const html = await axios.get(url);
+
+    let $ = cheerio.load(html.data);
+    $ = cheerio.load($(domInfo.from).html());
+
+    $(domInfo.remove).remove();
+
+    if (domInfo.unwrap) {
+        $(domInfo.unwrap).each(function () {
+            let $p = $(this).parent();
+            $(this).insertAfter($(this).parent());
+            $p.remove();
+        });
+    }
+
+    const elements = ['h1', 'h2', 'h3', 'p', 'code', 'img', 'ul', 'ol', 'li', 'a', 'blockquote', 'table'];
+
+    let doms = {};
+    elements.forEach(element => {
+        doms[element] = domInfo[element];
+    });
+
+    Object.entries(doms).forEach(entry => {
+        if (!entry[1]) {
+            delete doms[entry[0]];
+        }
+    });
+
+    const result = {};
+    elements.forEach(element => {
+        result[element] = 0;
+    });
+
+    Object.entries(doms).forEach(entry => {
+        $(entry[1]).each(() => {
+            result[entry[0]]++;
+        });
+    });
+
+    return result;
+};
+
+class DomCrawler {
+    constructor() {
+    }
+
+    async crawlDom(url, domInfo) {
+        return crawlDom(url, domInfo);
+    }
+}
+
+let domCrawler = new DomCrawler();
+
+module.exports = domCrawler;
--- src/services/dom/DomService.js
+++ src/services/dom/DomService.js
@@ -1,7 +1,6 @@
-import cheerio from 'cheerio';
 import Dom from '../../models/Dom';
 import BlogService from "../blog/BlogService";
-import axios from 'axios';
+import DomCrawler from "../../modules/DomCrawler"
 import {DatabaseError, DuplicatedPostUrlExistsError} from "../error/error";
 import {HTMLParseError, NotExistsDomError, NotExistsUnscoredDomError} from "./error/error";
 import '@babel/polyfill';
@@ -48,7 +47,7 @@ const saveDom = async (dom) => {
         throw new DuplicatedPostUrlExistsError();
     }
 
-    const elementsCount = await parseHTML(dom.url, savedBlog.elements)
+    const elementsCount = await DomCrawler.crawlDom(dom.url, savedBlog.elements)
         .catch(() => {
             throw new HTMLParseError();
         });
@@ -69,49 +68,6 @@ const existsUrl = (url) => {
             throw new DatabaseError(error);
         })
         .then(savedDom => !!savedDom);
-};
-
-const parseHTML = async (url, domInfo) => {
-    const html = await axios.get(url);
-
-    let $ = cheerio.load(html.data);
-    $ = cheerio.load($(domInfo.from).html());
-
-    $(domInfo.remove).remove();
-
-    if (domInfo.unwrap) {
-        $(domInfo.unwrap).each(function () {
-            let $p = $(this).parent();
-            $(this).insertAfter($(this).parent());
-            $p.remove();
-        });
-    }
-
-    const elements = ['h1', 'h2', 'h3', 'p', 'code', 'img', 'ul', 'ol', 'li', 'a', 'blockquote', 'table'];
-
-    let doms = {};
-    elements.forEach(element => {
-        doms[element] = domInfo[element];
-    });
-
-    Object.entries(doms).forEach(entry => {
-        if (!entry[1]) {
-            delete doms[entry[0]];
-        }
-    });
-
-    const result = {};
-    elements.forEach(element => {
-        result[element] = 0;
-    });
-
-    Object.entries(doms).forEach(entry => {
-        $(entry[1]).each(() => {
-            result[entry[0]]++;
-        });
-    });
-
-    return result;
 };
 
 const findDom = async ({scored, fromDate, endDate} = {}) => {
