dd4d73ad0ea2ee663e7b9966daab2f05e34df0af
skygl <jupiter9501@gmail.com>
2020-05-06 15:42:19 +0900

    feat: Slack 세팅을 해주는 SlackClient 추가

---
+++ src/config/slackclient.js
@@ -0,0 +1,23 @@
+import './env';
+import {WebClient} from "@slack/web-api";
+
+const SLACK_TOKEN = process.env.SLACK_TOKEN;
+const SLACK_CHANNEL_ID = process.env.SLACK_CHANNEL_ID;
+
+class SlackClient {
+
+    constructor() {
+        this.client = new WebClient(SLACK_TOKEN);
+    }
+
+    postRecommendsToSlackChannel(blocks) {
+        return this.client.chat.postMessage({
+            blocks: blocks,
+            channel: SLACK_CHANNEL_ID
+        });
+    }
+}
+
+const slackClient = new SlackClient();
+
+module.exports = slackClient;
